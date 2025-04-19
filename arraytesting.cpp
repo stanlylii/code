7ea4d7d1b338a82bd4d1f415b6b4812a837e176e
stanlylii <stanleyli361@gmail.com>
2025-04-19 11:16:24 -0700

    shapes

--- arraytesting.cpp
+++ arraytesting.cpp
@@ -13,15 +13,36 @@ void square(int row, int level){
 void hardtriangle(int level){
     char star = '*', spaces = ' ';
     for (int l=0; l < level; l++){//sdf
-        for (int sp=level-1; sp > 0;sp--){
-            cout <<  spaces;
+        for (int sp = level-l-1; sp > 0;sp--){
+            cout << spaces;
         }
-        for (int st=2*l-1; st > level*2){
-
+        for (int st=2*l+1; st>0; st--) {
+            cout << star;
         }
+        cout<<endl;
+    }
+}
+void bottomltriangle(int level){
+    char star = '*';
+    for (int l=0; l < level; l++){
+        for (int st=2*l+1; st>0; st--) {
+            cout << star;
+        }
+        cout<<endl;
+    }
+}
+void topltriangle(int level){
+    char star = '*';
+    for (int l=0; l < level; l++){
+        for (int st=2*level-2*l-1; st>0; st--) {
+            cout << star;
+        }
+        cout<<endl;
     }
 }
 int main() {
-    square(10,10);
-    hardtriangle(4);
+    // square(10,10);
+    // hardtriangle(50);
+    // bottomltriangle(6);
+    topltriangle(6);
 }
